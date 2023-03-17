cfbbf5862a6a5519e4ab9452644885ce13ae993e
vennila-3 <113087858+vennila-3@users.noreply.github.com>
2023-03-17 16:16:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function sub(){
+    let b=Number(document.getElementById('convert').value);
+    let name=(document.getElementById('name').value);
+
+if(b>=18){
+    alert(name+" You Are Eligible For Voting")
+}
+else{
+alert(name+" You Are Not Eligible For Voting")
+}
+}
